6fcd49f3f42ab23ab07d7aa4d52f4dce0a102361
matthes <leuffen@continue.de>
2018-07-19 15:42:45 +0200

    initial

---
+++ template/base-install-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+trap 'on_error $LINENO' ERR;
+PROGNAME=$(basename $0)
+
+function on_error () {
+    echo "Error: ${PROGNAME} on line $1" 1>&2
+    exit 1
+}
+
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+
+
+# Bug in Ubuntu18.04. You have to install tzdata standalone first!
+apt-get install -y --no-install-recommends tzdata
+apt-get install -y php7.2-cli curl zip composer vim nano git curl sudo
+
