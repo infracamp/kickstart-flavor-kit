667e4b9c66eeef2f46bba61daab8671edcc41c05
Matthias <m@tth.es>
2018-08-01 16:16:34 +0200

    Add whois to install scripts (to have mkpasswd preinstalled)

--- template/base-install-ubuntu.sh
+++ template/base-install-ubuntu.sh
@@ -17,5 +17,6 @@ apt-get update
 
 # Bug in Ubuntu18.04. You have to install tzdata standalone first!
 apt-get install -y --no-install-recommends tzdata
-apt-get install -y php7.2-cli curl zip composer vim nano git curl sudo
+apt-get install -y php7.2-cli curl zip composer vim nano git curl sudo whois
 
+# (whois: offers mkpasswd command)
