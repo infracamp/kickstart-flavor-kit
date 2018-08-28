5b0dcfc2b21f8809462cab3a1dc69e1fccb02ce0
Matthias <m@tth.es>
2018-08-28 18:20:34 +0200

    added bash-completion

--- template/base-install-ubuntu.sh
+++ template/base-install-ubuntu.sh
@@ -17,6 +17,6 @@ apt-get update
 
 # Bug in Ubuntu18.04. You have to install tzdata standalone first!
 apt-get install -y --no-install-recommends tzdata
-apt-get install -y php7.2-cli curl zip composer vim nano git curl sudo whois
+apt-get install -y php7.2-cli curl zip composer vim nano git curl sudo whois bash-completion
 
 # (whois: offers mkpasswd command)
