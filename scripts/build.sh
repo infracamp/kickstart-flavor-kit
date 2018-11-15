a68e0f896649afe8e1517c2930adcd196f13316f
Matthias <m@tth.es>
2018-11-15 11:41:12 +0100

    fix command line for root

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,7 +28,7 @@ gpasswd -a user adm
 # Set Color Prompt
 #PROMPT='export PROMPT_COMMAND='\''if [ `whoami` != "root" ] ;  then echo -ne "\e[0m\e[95m${DEV_TTYID}\e[0m`whoami`@\e[1;33m${DEV_CONTAINER_NAME}:\e[0m${PWD}$ "; else echo -ne "\e[101m\e[95m${DEV_TTYID}`whoami`@\e[1;33m${DEV_CONTAINER_NAME}:\e[0m${PWD}$ "; fi;'\'' '
 ##echo $PROMPT >> /root/.bashrc
-echo 'export PS1="\[\033[48;5;9m\]\u\[$(tput sgr0)\]\[\033[48;5;-1m\]@\[$(tput sgr0)\]\[\033[38;5;42m\]$DEV_CONTAINER_NAME[$(tput sgr0)\]\[\033[38;5;8m\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;21m\]\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;8m\]\\$\[$(tpu sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"' >> root/.bashrc
+echo 'export PS1="\u@\[$(tput sgr0)\]\[\033[38;5;42m\]$DEV_CONTAINER_NAME\[$(tput sgr0)\]\[\033[38;5;8m\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;21m\]\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;8m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"' >> root/.bashrc
 echo '' >> /root/.bashrc
 
 #echo $PROMPT >> /home/user/.bashrc
