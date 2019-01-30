cece9befdb87e9a618cde3d9351cf06b12976812
Matthias <m@tth.es>
2019-01-30 14:48:19 +0100

    added locale gen

--- template/base-install-ubuntu.sh
+++ template/base-install-ubuntu.sh
@@ -17,6 +17,8 @@ apt-get update
 
 # Bug in Ubuntu18.04. You have to install tzdata standalone first!
 apt-get install -y --no-install-recommends tzdata
-apt-get install -y php7.2-cli curl zip composer vim nano git curl sudo whois bash-completion pwgen netcat
+apt-get install -y php7.2-cli curl zip composer vim nano git curl sudo whois bash-completion pwgen netcat locales
+
+locale-gen en_US.UTF-8
 
 # (whois: offers mkpasswd command)
