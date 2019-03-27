bb56859f8b7faf432357fe3c544479d619a02963
Matthias <m@tth.es>
2019-03-27 14:05:35 +0100

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,35 +24,26 @@ composer install -d /kickstart/flavorkit/lib/kicker
 useradd -s /bin/bash --create-home user
 # Add user to admin group
 gpasswd -a user adm
-
-# Set Color Prompt
-#PROMPT='export PROMPT_COMMAND='\''if [ `whoami` != "root" ] ;  then echo -ne "\e[0m\e[95m${DEV_TTYID}\e[0m`whoami`@\e[1;33m${DEV_CONTAINER_NAME}:\e[0m${PWD}$ "; else echo -ne "\e[101m\e[95m${DEV_TTYID}`whoami`@\e[1;33m${DEV_CONTAINER_NAME}:\e[0m${PWD}$ "; fi;'\'' '
-##echo $PROMPT >> /root/.bashrc
-echo 'export PS1="\u@\[$(tput sgr0)\]\[\033[38;5;42m\]$DEV_CONTAINER_NAME\[$(tput sgr0)\]\[\033[38;5;8m\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;21m\]\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;8m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"' >> /root/.bashrc
-echo '' >> /root/.bashrc
-echo 'export GIT_EDITOR=vim' >> /root/.bashrc
-
-#echo $PROMPT >> /home/user/.bashrc
-#echo 'export PS1=""' >> /home/user/.bashrc
-echo 'export PS1="\u@\[$(tput sgr0)\]\[\033[38;5;42m\]$DEV_CONTAINER_NAME\[$(tput sgr0)\]\[\033[38;5;8m\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;21m\]\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;8m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"' >> /home/user/.bashrc
-
-echo "cd /opt" >> /home/user/.bashrc
-echo 'export GIT_EDITOR=vim' >> /home/user/.bashrc
-
 chown user:root /opt
+
 
 ## Remove secure_path (otherwise $PATH will be resetted with each sudo call)
 echo "`cat /etc/sudoers | grep -v "secure_path"`" > /etc/sudoers
 
-echo 'export PATH="$PATH:/home/user/.composer/vendor/bin"' >> /home/user/.bashrc
 
-## Load kick environment
+cat <<\EOF >> /home/user/.bashrc
+## Added from kickstart-flavor-kit build.sh (user-section):
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export GIT_EDITOR=vim
+export PATH="$PATH:/home/user/.composer/vendor/bin"
 
-echo 'export LC_ALL=en_US.UTF-8' >> /home/user/.bashrc
-echo 'export LANG=en_US.UTF-8' >> /home/user/.bashrc
-echo "export LANGUAGE=en_US.UTF-8" >> /home/user/.bashrc
+export PS1="\u@\[$(tput sgr0)\]\[\033[38;5;42m\]$DEV_CONTAINER_NAME\[$(tput sgr0)\]\[\033[38;5;8m\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;21m\]\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;8m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
 
-echo <<EOF >> /home/user/.bashrc
+## Change Dir to /opt
+cd /opt
+
 envtoset=`kick kick_to_env`
 echo "kick_to_env raw: $envtoset"
 ## Evaluate and replace $PATH in envtoset
@@ -60,7 +51,21 @@ eval envtoset_parsed="\"$envtoset\""
 export $envtoset_parsed
 EOF
 
-echo <<EOF >> /root/.bashrc
+cat <<\EOF >> /root/.bashrc
+## Added from kickstart-flavor-kit build.sh (root-section):
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export GIT_EDITOR=vim
+export PATH="$PATH:/root/.composer/vendor/bin"
+
+export PS1="\u@\[$(tput sgr0)\]\[\033[38;5;42m\]$DEV_CONTAINER_NAME\[$(tput sgr0)\]\[\033[38;5;8m\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;21m\]\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;8m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
+
+
+## Change Dir to /opt
+cd /opt
+
+
 envtoset=`kick kick_to_env`
 echo "kick_to_env raw: $envtoset"
 ## Evaluate and replace $PATH in envtoset
